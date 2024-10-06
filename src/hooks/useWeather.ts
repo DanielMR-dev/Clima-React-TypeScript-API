5512fbcbed2a0a03537abf7170a4f77adaab3d3d
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-10-05 20:09:06 -0500

    268. Que son las variables de Entorno

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -5,11 +5,12 @@ export default function useWeather() {
 
     const fetchWeather = async (search: SearchType) => {
 
-        const appId = 'e05b61a09fb34f75cf94442551e06e92';
+        const appId = import.meta.env.VITE_API_KEY;
         try {
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
 
             const {data} = await axios.get(geoUrl); // Se hace desctrocturing a la variable usando {data} para ingresar a data en el llamado de la API
+            console.log(data);
 
         } catch (error) {
             console.log(error);
