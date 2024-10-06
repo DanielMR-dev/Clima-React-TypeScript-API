c4010c40d1797ddd5b66a80d9fa8600d7c7e3a80
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-10-06 14:31:33 -0500

    271. Tipar Resultados de una API - Type Guards

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,6 +1,18 @@
 import axios from 'axios';
 import { SearchType, Weather } from '../types';
 
+// TYPE GUARD O ASSERTION
+function isWeatherRespone(weather : unknown) : weather is Weather {
+    return (
+        Boolean(weather) && 
+        typeof weather === 'object' &&
+        typeof (weather as Weather).name === 'string' &&
+        typeof (weather as Weather).main.temp === 'number' &&
+        typeof (weather as Weather).main.temp_max === 'number' &&
+        typeof (weather as Weather).main.temp_min === 'number'
+    );
+};
+
 export default function useWeather() {
 
     const fetchWeather = async (search: SearchType) => {
@@ -15,14 +27,21 @@ export default function useWeather() {
             const lon = data[0].lon;
 
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+             
+            // Castear el tpye
+            // const {data: weatherResult} = await axios<Weather>(weatherUrl);
 
-            const {data: weatherResult} = await axios<Weather>(weatherUrl);
-
+            // Type Guards
+            const {data: weatherResult} = await axios(weatherUrl);
+            const result = isWeatherRespone(weatherResult);
+            if(result) {
+                
+            };
 
         } catch (error) {
             console.log(error);
-        }
-    }
+        };
+    };
 
     return {
         fetchWeather
