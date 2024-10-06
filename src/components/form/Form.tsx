f9513d959276a16a8dedbb75955dae2f9536c923
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-10-05 19:33:16 -0500

    266. Creando un Hook para Consultar el Clima

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -4,7 +4,12 @@ import { countries } from "../../data/countries";
 import styles from './Form.module.css';
 import Alert from "../Alert/Alert";
 
-export default function Form() {
+
+type FormProps = {
+    fetchWeather: () => void;
+}
+
+export default function Form({fetchWeather} : FormProps) {
 
     const [search, setSearch] = useState<SearchType>({
         city: '',
@@ -26,6 +31,7 @@ export default function Form() {
             setAlert('Todos los campos son obligatorios');
             return;
         };
+        fetchWeather();
     };
 
     return (
